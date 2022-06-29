95dee50c28c2889d148e188fd90212391957d2ff
bytes <bytes@lattice.xyz>
2022-06-29 17:51:21 -0400

    feat(src): add no-metric metric funcs

--- dist/prefabs/Metrics.d.ts
+++ dist/prefabs/Metrics.d.ts
@@ -1,3 +1,5 @@
 import * as Types from "../types";
+export declare const noMetric: Types.MetricsFunc;
+export declare const noMetricNoWait: Types.MetricsFunc;
 export declare const timeIt: Types.MetricsFunc;
 export declare const txInfo: Types.MetricsFunc;
