bceab1bcd15f788693e62a6aa84349a08baaeb2a
bytes <bytes@lattice.xyz>
2023-03-01 17:26:10 +0100

    refactor(example): add comments and script

--- examples/example.ts
+++ examples/example.ts
@@ -14,14 +14,20 @@ async function main() {
   type C = P;
   type M = C;
 
+  // A params function that returns the call index
   const paramsFunc: ParamsFunc<number> = async (callContext, testContext) => {
     return callContext.callIdx;
   };
+  // A call function that returns the raw param without any action
+  // Usually, this would be a call to an RPC endpoint
   const callFunc: CallFunc<P, C> = async (params, callContext, testContext) => {
     return params;
   };
 
-  const reports = [new ReportDataArray<number, P, M>("test")];
+  // Report the raw metrics (which will be the params, which will be the call index) as an array
+  const reports = [new ReportDataArray<number, P, M>("example")];
+  
+  // Run the stress test with 10 calls
   const _stressTestConfig: Partial<StressTestConfig> = {
     nCalls: 10,
   };
@@ -37,7 +43,7 @@ async function main() {
       log: false,
     }
   );
-
+  // Output will be an array of 10 numbers, each number being the call index
   console.log(output);
 }
 
